f1ccf16af3c73c174ee483834c5870530bd9864c
Wellington Carlos Massola <wellingtonmassola@outlook.com>
2022-09-20 18:47:58 -0300

    initial commit
    A README and a default project created with the followin commands:
    dotnet new sln
    dotnet new maui -o src/Maui.VSCode
    dotnet sln add src/Maui.VSCode

---
+++ README.md
@@ -0,0 +1,3 @@
+# Test project with .NET MAUI
+
+This is a test project about debugging on linux with .NET MAUI.
---
+++ src/Maui.VSCode/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Maui.VSCode;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
